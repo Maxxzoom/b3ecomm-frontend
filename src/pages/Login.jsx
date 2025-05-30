dcff0d44da670d90f7785851b351dc2ffcc99721
Umeshgardas23 <ugtech23@gmail.com>
2025-05-30 12:02:26 +0530

    create login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import API from "../utils/axios";
 
-const Login = () => {
+const Register = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  console.log(form);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await API.post("/users/login", form);
+      console.log(res);
+      alert("Login Successful");
+      navigate("/");
+    } catch (error) {
+      setMessage(error.response?.data?.msg || "Registration failed");
+    }
+  };
   return (
     <div>
-      Login
-    </div>
-  )
-}
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          onChange={handleChange}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
 
-export default Login
+      {message}
+    </div>
+  );
+};
+
+export default Register;
